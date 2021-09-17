35ddc205b42c09632b366d4082ee34743e121e1b
Shubh Nisar <shubh_ubuntu@shubh.ubuntu-Lenovo-ideapad-330S-15IKB>
2021-09-17 23:25:28 +0530

    Navbar.jsx init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import { GlobalProvider } from "./GlobalContext";
 import SignInSide from "./screens/signIn/signIn";
 import SignUp from "./screens/signUp/signUp";
 
+import Navbar from './components/navbar/Navbar';
+
 import Routes from "./Routes";
 
 const App = (props) => {
   return (
     <GlobalProvider>
+      <Navbar />
       <Routes />
     </GlobalProvider>
   );
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,7 @@ import { Router, Switch, Route, BrowserRouter } from "react-router-dom";
 import Temp from "./admin/Temp";
 import SignInSide from "./screens/signIn/signIn";
 import SignUp from "./screens/signUp/signUp";
+import Home from "./screens/home/Home";
 
 const Routes = (props) => {
   const history = createBrowserHistory();
@@ -15,7 +16,7 @@ const Routes = (props) => {
         <Route path="/admin" component={Temp} exact />
         <Route path="/login" component={SignInSide} exact />
         <Route path="/register" component={SignUp} exact />
-
+        <Route path="/home" component={Home} exact />
       </Switch>
     </BrowserRouter>
   );
