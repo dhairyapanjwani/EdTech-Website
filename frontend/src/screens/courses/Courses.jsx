6e280934948df512762c42e5890fc87b3e7ee50f
Shubh Nisar <shubh_ubuntu@shubh.ubuntu-Lenovo-ideapad-330S-15IKB>
2021-09-18 23:33:05 +0530

    Theme Switching via Tailwind

---
+++ frontend/src/screens/courses/Courses.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navbar from '../../components/navbar/Navbar';
+
+const Courses = props => {
+    return(
+        <div>
+            <Navbar />
+            
+        </div>
+    );
+};
+
+export default Courses;
