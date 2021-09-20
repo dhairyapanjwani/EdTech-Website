bfabbab95310a2500337d61f5015ef05397e21e3
khushbuparmar25 <66319265+khushbuparmar25@users.noreply.github.com>
2021-09-21 01:00:05 +0530

    partial homepage commit

--- frontend/src/screens/home/Home.jsx
+++ frontend/src/screens/home/Home.jsx
@@ -3,18 +3,34 @@ import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import './home.css';
 // import video from './live_background.mp4'
+import home_illustrations from './a1.svg'
 
 const Home = () => {
     return (
-        <div>
+        <div className="bg-gray-50 dark:bg-gray-800" >
             {/* <video src={video} width="750" height="500" controls/> */}
             <Navbar />
-            <h1>
+            
+            <div class="grid grid-cols-6 gap-4">
+                <div class="col-start-3 col-end-7 ">
+                    <img src={home_illustrations} alt="" class='object-scale-down h-96 w-3/5 -mx-8' />
+                </div>
+                <div class="col-start-1 col-end-7 ">
+                <div class="container mx-auto">
+                <div class="flex flex-col text-center w-full">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font text-black font-bold dark:text-white">The world's largest selection of courses</h1>
+                </div>
+                </div>
+                    <button class=" button-styles flex lg:w-70 mx-auto mt-6 mb-6 text-gray-800 py-2 px-12 border-gray-500 border-2 focus:outline-none hover:bg-gray-300 text-lg dark:text-gray-300 dark:hover:bg-gray-600">Browse Courses</button>
+                </div>
+            </div>
+
+            <div className="border-b-2 border-dashed border-indigo-500" ></div>            
             <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
+                <div class="container px-5 py-10 mx-auto">
                     <div class="flex flex-col text-center w-full mb-20">
-                    <h2 class="text-xs text-blue-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Master Cleanse Reliac Heirloom</h1>
+                    <h2 class="text-xs text-blue-500 tracking-widest font-medium title-font mb-1">Expert courses</h2>
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 dark:text-gray-100">Explore a variety of courses and gain new skills</h1>
                     </div>
                     <div class="flex flex-wrap -m-4">
                     <div class="p-4 md:w-1/3">
@@ -80,10 +96,59 @@ const Home = () => {
                         </div>
                         </div>
                     </div>
+                    <button class=" button-styles flex lg:w-70 mx-auto mt-6 mb-16 text-gray-800 py-2 px-12 border-gray-500 border-2 focus:outline-none text-lg hover:bg-gray-300 text-lg dark:text-gray-300 dark:hover:bg-gray-600">View more courses</button>
                     </div>
+                    
+                    
                 </div>
-                </section>
-            </h1>
+            </section>
+            <div class="testimonals-bg dark:bg-gray-600">
+
+            <h1 class="text-3xl font-medium title-font text-gray-900 mb-5 text-center px-5 py-10 mx-auto dark:text-gray-100">Testimonials</h1>
+
+                <div class="flex flex-wrap -m-4">
+                
+                    <div class="p-4 md:w-1/3">
+                        <div class="flex rounded-lg h-full bg-white p-8 flex-col">
+                            <div class="h-full text-center">
+                                <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302" />
+                                <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                                <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
+                                <p class="text-gray-500">Senior Product Designer</p>
+                            </div>
+                        
+                        </div>
+                    </div>
+
+                    <div class="p-4 md:w-1/3">
+                        <div class="flex rounded-lg h-full bg-white p-8 flex-col">
+                            <div class="h-full text-center">
+                                <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302" />
+                                <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                                <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
+                                <p class="text-gray-500">Senior Product Designer</p>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/3">
+                        <div class="flex rounded-lg h-full bg-white p-8 flex-col">
+                            <div class="h-full text-center">
+                                <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302" />
+                                <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                                <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
+                                <p class="text-gray-500">Senior Product Designer</p>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    
+                </div>               
+                
+            </div>
         </div>
     );
 };
