c822ae4c91013c857755e2f41d5f65f03c0293d2
Shubh Nisar <shubh_ubuntu@shubh.ubuntu-Lenovo-ideapad-330S-15IKB>
2021-09-22 13:04:08 +0530

    Reports init

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -11,6 +11,7 @@ import Example from "../src/leaderBoard/leader.js";
 import Quiz from "./screens/quiz/Quiz";
 import UploadScore from "./admin/screens/upload/UploadCourse";
 import NotFound from "./screens/404/NotFound";
+import Reports from "./screens/report/Reports";
 
 const Routes = (props) => {
   const history = createBrowserHistory();
@@ -28,6 +29,7 @@ const Routes = (props) => {
         <Route path="/leaderBoard" component={Example} exact />
         <Route path="/video/:id" component={VideoScreen} exact />
         <Route path="/admin/upload/course" component={UploadScore} exact />
+        <Route path="/reports" component={Reports} exact />
         <Route path="/" component={NotFound} />
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/screens/report/Reports.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+
+const Reports = props => {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                Reports.jsx
+            </div>
+        </div>
+    );
+};
+
+export default Reports;
