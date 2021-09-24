338a065d160b11ef560fc467b939873e9a7286a9
rishabhpatil__ <rishabhpatil8108@gmail.com>
2021-09-24 14:33:11 +0530

    dataset

--- python-ml/generate.py
+++ python-ml/generate.py
@@ -7,7 +7,25 @@ import random
 with open('test_data.csv', 'a',newline="") as f_object:
     for i in range(10000):
         category=random.randint(1,5)
-        List=[i,category,random.randint(1,5),random.randint(1,20),random.randint(1,500),random.randint(1,1000),category]
+        rating=random.randint(1,5)
+        video_count=random.randint(1,20)
+        comment_count=random.randint(1,2000)
+        likes=random.randint(1,3000)
+        score=0
+        if comment_count in range(0,400) and likes in range(0,600):    
+            score=1
+        elif comment_count in range(400,800) and likes in range(600,1200):
+            score=2
+        elif comment_count in range(800,1200) and likes in range(1200,1800):
+            score=3
+        elif comment_count in range(1200,1600) and likes in range(1800,2400):
+            score=4
+        elif comment_count in range(1600,2000) and likes in range(2400,3000):
+            score=5
+        else:
+            score=random.randint(1,5)
+
+        List=[i,category,rating,video_count,comment_count,likes,score]
         writer_object = writer(f_object)
         writer_object.writerow(List)
     f_object.close()
