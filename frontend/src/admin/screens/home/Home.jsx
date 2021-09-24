8c0ffab53e18e83244577271d407e4710160a615
hershvitekar <hershvitekar@gmail.com>
2021-09-24 14:30:33 +0530

    React - The Complete Guide video update

--- frontend/src/admin/screens/home/Home.jsx
+++ frontend/src/admin/screens/home/Home.jsx
@@ -134,7 +134,7 @@ export default function Example() {
 
 
 <div>
-                      <label className="block text-sm font-medium text-gray-700">Upload study material</label>
+                      <label className="block text-sm font-medium text-gray-700">Upload video</label>
                       <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                         <div className="space-y-1 text-center">
                           <svg
@@ -174,7 +174,7 @@ export default function Example() {
 
 
                                         <div>
-                      <label className="block text-sm font-medium text-gray-700">Upload study material</label>
+                      <label className="block text-sm font-medium text-gray-700">Upload thumbnail</label>
                       <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                         <div className="space-y-1 text-center">
                           <svg
